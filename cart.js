90545b1db5857ca7aa78a326746882b76f5c29ce
Saad-IUT <saadbinjohir@gmail.com>
2021-01-09 23:50:55 +0600

    Done

--- cart.js
+++ cart.js
@@ -1,28 +1,23 @@
 const { db } = require('./admin')
 const { checkSession } = require('./validators')
 
-// Add Product
+// Add Order
 exports.addItem = async (req, res) => {
-  const product = req.body
-  const valid = await checkSession(product.name)
-  if (valid) {
-    const newOrder = {
-      name: product.name,
-      categoryId: product.categoryId,
-      categoryName: '',
-      averageRating: null,
-      numberOfRaters: 0,
-    }
-    db.collection('products')
-      .add(newProduct)
-      .then(() => {
-        res.status(201).json({ message: 'Product added successfully' })
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'something went wrong' })
-        console.error(err)
-      })
-  } else {
-    return res.status(400).json({ productName: 'Duplicate' })
-  }
+  console.log(req)
+  // const newOrder = {
+  //   name: product.name,
+  //   categoryId: product.categoryId,
+  //   categoryName: '',
+  //   averageRating: null,
+  //   numberOfRaters: 0,
+  // }
+  // db.collection('sessions')
+  //   .add(newOrder)
+  //   .then(() => {
+  //     res.status(201).json({ message: 'Product added successfully' })
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ error: 'something went wrong' })
+  //     console.error(err)
+  //   })
 }
