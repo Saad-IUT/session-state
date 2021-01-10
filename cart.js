aff7d90eef1d4694113e819760b6a470fc48b56e
Saad-IUT <saadbinjohir@gmail.com>
2021-01-10 12:50:02 +0600

    Done

--- cart.js
+++ cart.js
@@ -2,21 +2,12 @@ const { db } = require('./admin')
 const { checkSession } = require('./validators')
 
 // Add Order
-exports.addItem = async (req, res) => {
-  console.log(req.sessionId)
-  const newOrder = {
-    [req.params.item]: 1,
-  }
-  db.collection('sessions')
-    .add(newOrder)
-    .then(doc => {
-      console.log(doc.id)
-      res.setHeader('session-id', doc.id)
-      console.log(res.header())
-      res.status(201).json({ message: 'Product added successfully' })
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'something went wrong' })
-      console.error(err)
-    })
+exports.addItem = (req, res) => {
+  let sessionId
+  if (res.getHeader('session-id')) {
+    sessionId = res.getHeader('session-id')
+  } else if (req.headers['session-id']) {
+    sessionId = req.headers['session-id']
+  } else console.error('Error while retrieving session')
+ 
 }
