6e97af57035bfd55b2cc36dcef55168c12965d66
Saad-IUT <saadbinjohir@gmail.com>
2021-01-10 20:04:46 +0600

    Done

--- cart.js
+++ cart.js
@@ -5,18 +5,7 @@ const fs = require('fs')
 
 // Add Item
 exports.addItem = async (req, res) => {
-  let sessionId
-  sessionId = file['session-id']
-  if (sessionId) {
-    req.headers['session-id'] = sessionId
-  } else {
-    sessionId = uuidv4()
-    res.setHeader('session-id', sessionId)
-    fs.writeFileSync('sessionId.json', `{"session-id":"${sessionId}"}`, err => {
-      if (err) return err
-      console.log('Saved!')
-    })
-  }
+  let sessionId = getSession(req, res)
   const sessionRef = db.doc(`sessions/${sessionId}`)
   sessionRef
     .get()
@@ -41,4 +30,33 @@ exports.addItem = async (req, res) => {
       console.error(err)
     })
 }
+// Get all screams
+exports.getCart = (req, res) => {
+  let sessionId = getSession(req, res)
+  const sessionRef = db.doc(`sessions/${sessionId}`)
+  sessionRef
+    .get()
+    .then(doc => {
+      return res.status(200).json(doc.data())
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ error: err.code })
+    })
+}
 
+const getSession = (req, res) => {
+  let sessionId
+  sessionId = file['session-id']
+  if (sessionId) {
+    req.headers['session-id'] = sessionId
+  } else {
+    sessionId = uuidv4()
+    res.setHeader('session-id', sessionId)
+    fs.writeFileSync('sessionId.json', `{"session-id":"${sessionId}"}`, err => {
+      if (err) return err
+      console.log('Saved!')
+    })
+  }
+  return sessionId
+}
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const app = require('express')()
 const jsonParser = require('body-parser').json()
-const { addItem } = require('./cart')
+const { addItem, getCart } = require('./cart')
 
 app.use(jsonParser)
 
 // Cart routes
 app.post('/cart/add/:item', addItem)
-// app.post('/test', postOneScream)
 // app.delete('/cart/remove/:item', removeItem)
 // app.delete('/cart/decrease/:item', decreaseItem)
-// app.get('/cart', getCart)
+app.get('/cart', getCart)
 
 //Testing
 app.get('/', (req, res) => {
